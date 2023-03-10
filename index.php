e38df1debc150db604d30a492a5a0bc508596ed5
Antoine Ouellette <2143781@carrefour.cegepvicto.ca>
2023-03-09 22:01:51 -0500

    Page d'accueil avec les activitées responsive et @media pour appareils mobiles

--- index.php
+++ index.php
@@ -9,29 +9,57 @@ require 'include/header-head.inc';
 ?>
 
 <!-- position absolue -->
-<div class="absolute-position-top-left flexbox flex-direction-row flex-align-items-center flex-row-gap-10px">
+<div class="absolute-position-title flexbox flex-direction-row flex-align-items-center flex-row-gap-10px">
     <img src="medias/server.svg" alt="Icone ailleurs"/>
     <h2 class="font-accent-color">Bienvenue à la vitrine de l'informatique</h2>
 </div>
 
 <!-- position normale -->
-<div class="flexbox flex-center-center stretch-all">
+<div class="flexbox flex-center-center stretch-all" id="div-fill">
     <div class="flexbox flex-justify-content-center">
-        <div class="block flexbox flex-direction-colomn flex-column-gap-20px no-stretch">
-            <h1 class="padding-horizontal-30px">Êtes-vous devant la TV de la vitrine?</h1>
-            <div class="flexbox flex-direction-row flex-row-gap-30px flex-grow-horizontal width-40vw">
+        <div class="block flexbox flex-direction-column flex-align-items-center flex-column-gap-20px no-stretch">
+            <h1 class="padding-horizontal-30px no-stretch">Activités interactives</h1>
+            <div id="grid-container-activities" class="width-50vw">
 
                 <a class="Rounded-Rectangle flexbox flex-center-center">
-                    <div class="flexbox flex-direction-row no-stretch">
-                        <img src="medias/tv.svg" alt="Icone TV"/>
-                        <p class="font-accent-color">Devant la TV</p>
+                    <div class="flexbox flex-direction-column flex-align-items-center no-stretch">
+                        <img src="medias/snowflake.svg" alt="Icone flocon de neige" class="no-stretch"/>
+                        <p class="font-accent-color text-align-center no-stretch">Fractales</p>
                     </div>
                 </a>
 
                 <a class="Rounded-Rectangle flexbox flex-center-center">
-                    <div class="flexbox flex-direction-row no-stretch">
-                        <img src="medias/logout.svg" alt="Icone ailleurs"/>
-                        <p class="font-accent-color">Ailleurs</p>
+                    <div class="flexbox flex-direction-column flex-align-items-center no-stretch">
+                        <img src="medias/gaming-pad.svg" alt="Icone manette de jeu" class="no-stretch"/>
+                        <p class="font-accent-color text-align-center no-stretch">Jeu Pong</p>
+                    </div>
+                </a>
+
+                <a class="Rounded-Rectangle flexbox flex-center-center">
+                    <div class="flexbox flex-direction-column flex-align-items-center no-stretch">
+                        <img src="medias/globe.svg" alt="Icone globe" class="no-stretch"/>
+                        <p class="font-accent-color text-align-center no-stretch">API</p>
+                    </div>
+                </a>
+
+                <a class="Rounded-Rectangle flexbox flex-center-center">
+                    <div class="flexbox flex-direction-column flex-align-items-center no-stretch">
+                        <img src="medias/locked-file.svg" alt="Icone fichier cadenas" class="no-stretch"/>
+                        <p class="font-accent-color text-align-center no-stretch">Capture the Flag</p>
+                    </div>
+                </a>
+
+                <a class="Rounded-Rectangle flexbox flex-center-center">
+                    <div class="flexbox flex-direction-column flex-align-items-center no-stretch">
+                        <img src="medias/key.svg" alt="Icone clé" class="no-stretch"/>
+                        <p class="font-accent-color text-align-center no-stretch">Cryptographie</p>
+                    </div>
+                </a>
+
+                <a class="Rounded-Rectangle flexbox flex-center-center">
+                    <div class="flexbox flex-direction-column flex-align-items-center no-stretch">
+                        <img src="medias/power.svg" alt="Icone alimentation" class="no-stretch"/>
+                        <p class="font-accent-color text-align-center no-stretch">Machines virtuelles</p>
                     </div>
                 </a>
 
